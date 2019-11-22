84a6e99318984fb029c20e395a07282f7c7b162d
mohammednadeem01 <mohdnadeem01786@gmail.com>
2019-11-22 10:08:33 +0530

    This is my first commit

---
+++ swap.js
@@ -0,0 +1,76 @@
+/*
+METHOD 1
+
+var a = 2;
+var b = 3;
+var temp;
+temp = a;
+a = b;
+b = temp;
+console.log(a,b);
+*/
+
+
+/*
+
+METHOD 2
+(ADDITION AND SUBTRACTION)
+
+var a =2;
+var b = 3;
+a= a + b;
+b = a - b;
+a = a - b;
+
+console.log(a,b);
+*/
+
+/*
+METHOD 3
+(MANUAL SWAP)
+
+let a = 2, b = 3;
+[a,b] = [b,a];
+console.log(a,b);
+
+*/
+
+/*
+METHOD 4
+(XOR)
+
+let a = 2, b = 3;
+a = a^b;
+b = a^b;
+a = a^b;
+
+console.log(a,b);
+*/
+
+
+/*
+METHOD 5
+(MULTIPLICATION AND DIVISION)
+
+var a = 2;
+var b = 3;
+a = a*b; //a=6
+b = a/b; //b=2
+a = a/b; //a=3
+
+console.log(a,b);
+*/
+
+
+/*
+METHOD 6-NEGATION-XOR
+
+
+let a = 2, b = 3;
+a = ~(a^b);
+b = ~(a^b);
+a = ~(a^b);
+
+console.log(a,b);
+
+*/
